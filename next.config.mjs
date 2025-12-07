3260fcaa3215834a14ea94e8735f515a7ea49d09
nawafsalha1998n <pemoral.official@gmail.com>
2025-12-07 23:07:32 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true
-  // ما في output: 'export' هون
+  // مهم: ما في output: 'export' هون
 };
 
 export default nextConfig;
