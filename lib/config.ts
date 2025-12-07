95e8b9792b37c7afe7321d819e670427e62a3486
nawafsalha1998n <pemoral.official@gmail.com>
2025-12-07 20:08:16 +0200

    Update config.ts

---
+++ lib/config.ts
@@ -0,0 +1,12 @@
+export const RESTAURANT_NAME = 'Sushi Highway';
+
+export const WHATSAPP_NUMBER_RAW = '009613823005';
+export const WHATSAPP_NUMBER_E164 = '9613823005';
+export const WHATSAPP_LINK = `https://wa.me/${WHATSAPP_NUMBER_E164}`;
+
+export const INSTAGRAM_URL =
+  'https://www.instagram.com/sushi.highway?igsh=cGpyeHdwYmo1NzNv';
+
+export const RESTAURANT_ADDRESS = 'Beirut, Lebanon';
+
+export const SITE_URL = 'https://sushi-highway.example.com'; // replace after deploy
