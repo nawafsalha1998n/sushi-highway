b42d932d980343fa22ea57b335c0940f6b54ad86
nawafsalha1998n <pemoral.official@gmail.com>
2025-12-11 22:11:26 +0200

    Update SplashScreen.tsx

--- components/SplashScreen.tsx
+++ components/SplashScreen.tsx
@@ -33,8 +33,8 @@ export default function SplashScreen() {
               <Image
                 src="/logo.png"
                 alt="Sushi Highway logo"
-                width={150}
-                height={150}
+                width={200}
+                height={200}
                 className="object-contain p-1.5"
                 priority
               />
@@ -51,6 +51,7 @@ export default function SplashScreen() {
     </AnimatePresence>
   );
 }
+
 
 
 
